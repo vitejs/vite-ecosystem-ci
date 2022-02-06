dc8a0776e27e70e5f79b1c2862fcd903ededa78a
aslemammad <zorofight94@gmail.com>
2022-02-06 18:16:44 +0330

    add hydrogen

---
+++ tests/hydrogen.ts
@@ -0,0 +1,11 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'Shopify/hydrogen',
+		build: 'build',
+		test: 'test'
+	})
+}
