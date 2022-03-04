58487e5800519ace5d210700688fc86e89017a04
Fran Dios <frankdiox@gmail.com>
2022-03-04 23:26:53 +0900

    fix: remove environment variable

--- tests/hydrogen.ts
+++ tests/hydrogen.ts
@@ -2,19 +2,10 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
-	// Enable terminal colors for snapshot testing.
-	// https://nodejs.org/api/cli.html#environment-variables
-	const initialValue = process.env.FORCE_COLOR
-	process.env.FORCE_COLOR = 'true'
-
-	try {
-		await runInRepo({
-			...options,
-			repo: 'Shopify/hydrogen',
-			build: 'build',
-			test: 'test:vite-ci'
-		})
-	} finally {
-		process.env.FORCE_COLOR = initialValue
-	}
+	await runInRepo({
+		...options,
+		repo: 'Shopify/hydrogen',
+		build: 'build',
+		test: 'test:vite-ci'
+	})
 }
