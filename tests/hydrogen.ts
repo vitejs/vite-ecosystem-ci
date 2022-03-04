862f5d0c861d7fba57995e6f9ede6cfcdb425c8b
Fran Dios <frankdiox@gmail.com>
2022-03-04 20:25:24 +0900

    fix: enable Hydrogen and fix terminal colors

--- tests/hydrogen.ts
+++ tests/hydrogen.ts
@@ -2,6 +2,10 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
+	// Enable terminal colors for snapshot testing.
+	// https://nodejs.org/api/cli.html#environment-variables
+	process.env.FORCE_COLOR = 'true'
+
 	await runInRepo({
 		...options,
 		repo: 'Shopify/hydrogen',
