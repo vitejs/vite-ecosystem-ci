e4722f730264eab30b041bfff8cfe311e92180e1
patak <matias.capeletto@gmail.com>
2022-03-07 13:10:23 +0100

    fix: master branch for vite-plugin-ssr and telefunc (#50)

--- tests/telefunc.ts
+++ tests/telefunc.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vikejs/telefunc',
+		branch: 'master',
 		beforeInstall: 'setup', // Needed for submodule initialization
 		build: 'build',
 		test: 'test'
--- tests/vite-plugin-ssr.ts
+++ tests/vite-plugin-ssr.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'brillout/vite-plugin-ssr',
+		branch: 'master',
 		beforeInstall: 'setup', // Needed for submodule initialization
 		build: 'build',
 		test: 'test'
