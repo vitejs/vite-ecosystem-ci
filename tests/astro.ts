1e146d7678b7f03a215f99469eb17f6fec15902d
dominikg <dominik.goepel@gmx.de>
2022-03-08 23:31:22 +0100

    feat: add astro

---
+++ tests/astro.ts
@@ -0,0 +1,11 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'withastro/astro',
+		build: 'build',
+		test: 'test'
+	})
+}
