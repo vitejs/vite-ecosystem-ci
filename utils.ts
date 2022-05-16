3cb6c4ece9dd62b3cd7367d1748853ebc206f3f7
Dominik G <dominik.goepel@gmx.de>
2022-05-16 23:24:59 +0200

    fix: disable strict-peer-dependencies for pnpm installs, update svelte script (#73)

--- utils.ts
+++ utils.ts
@@ -278,7 +278,7 @@ export async function applyPackageOverrides(
 
 	// use of `ni` command here could cause lockfile violation errors so fall back to native commands that avoid these
 	if (pm === 'pnpm') {
-		await $`pnpm install --prefer-frozen-lockfile --prefer-offline`
+		await $`pnpm install --prefer-frozen-lockfile --prefer-offline --strict-peer-dependencies false`
 	} else if (pm === 'yarn') {
 		await $`yarn install`
 	} else if (pm === 'npm') {
