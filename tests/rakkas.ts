0ffc175baea95e6e0bef5958da9fa28ba0ca1c23
Fatih Aygün <cyco130@gmail.com>
2022-05-30 09:53:08 +0300

    feat: add rakkas (#75)

---
+++ tests/rakkas.ts
@@ -0,0 +1,12 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'rakkasjs/rakkasjs',
+		branch: 'next',
+		build: 'build',
+		test: 'ci'
+	})
+}
