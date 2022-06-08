604945343db94cd597f9582cdfa3486accc57db4
Rom <github.com@brillout.com>
2022-06-08 11:33:53 +0200

    fix: use vite-plugin-ssr Vite 3 compatible branch (#80)

--- tests/vite-plugin-ssr.ts
+++ tests/vite-plugin-ssr.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'brillout/vite-plugin-ssr',
-		branch: 'master',
+		branch: '0.4.0',
 		beforeInstall: 'setup', // Needed for submodule initialization
 		build: 'build',
 		test: 'test'
