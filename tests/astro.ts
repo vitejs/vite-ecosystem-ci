09b080048e61768c4a7cdb0414c53f7d9d3d20db
patak <matias.capeletto@gmail.com>
2022-06-11 12:58:04 +0200

    Update astro.ts (#82)

--- tests/astro.ts
+++ tests/astro.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'withastro/astro',
+		branch: 'vite-3',
 		build: 'build:ci',
 		test: 'test:vite-ci'
 	})
