8b36d420f3a6ee46e030b5384fcbe82a15e8b42b
patak <matias.capeletto@gmail.com>
2022-06-13 10:58:22 +0200

    chore: point Nuxt to vite 3 branch

--- tests/nuxt-framework.ts
+++ tests/nuxt-framework.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'nuxt/framework',
+		branch: 'chore/vite-3',
 		build: 'build',
 		test: ['test:fixtures', 'test:types']
 	})
