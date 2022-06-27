3331d864928002b49aba7c52bf32a6d3085c798e
patak <matias.capeletto@gmail.com>
2022-06-27 16:13:19 +0200

    feat: point vitest to feat/vite-3 branch (#84)

--- tests/vitest.ts
+++ tests/vitest.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vitest-dev/vitest',
+		branch: 'feat/vite-3',
 		build: 'build',
 		test: 'test:run --allowOnly'
 	})
