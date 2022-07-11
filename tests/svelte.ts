06a2031048defd744a01811cb3c8796bdc82d112
Dominik G <dominik.goepel@gmx.de>
2022-07-11 18:12:24 +0200

    chore: ensure playwright browsers are installed before executing svel… (#96)

--- tests/svelte.ts
+++ tests/svelte.ts
@@ -18,6 +18,7 @@ export async function test(options: RunOptions) {
 			'@sveltejs/vite-plugin-svelte': `${pluginPath}/packages/vite-plugin-svelte`
 		},
 		build: 'build',
+		beforeTest: 'pnpm playwright install',
 		test: 'test'
 	})
 }
