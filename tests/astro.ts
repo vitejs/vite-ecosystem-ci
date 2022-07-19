dc0f89851a11c14f7d79c11f683b6e1cc3018f7d
bholmesdev <hey@bholmes.dev>
2022-07-19 11:08:02 -0400

    Move Astro ecosystem branch back to main

--- tests/astro.ts
+++ tests/astro.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'withastro/astro',
-		branch: 'vite-3',
+		branch: 'main',
 		build: 'build:ci',
 		test: 'test:vite-ci'
 	})
