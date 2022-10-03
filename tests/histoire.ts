eb9895d21798cd308b85b9c2a95553580f814d14
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-10-03 12:49:57 +0200

    feat: add histoire

---
+++ tests/histoire.ts
@@ -0,0 +1,12 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'histoire-dev/histoire',
+		branch: 'main',
+		build: 'build',
+		test: ['test', 'test:examples']
+	})
+}
