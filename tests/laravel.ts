9f1152bf83f10bce134d6b61f04db8d27eca4098
dominikg <dominik.goepel@gmx.de>
2022-10-06 11:39:50 +0200

    fix(laravel): set env to avoid check error

--- tests/laravel.ts
+++ tests/laravel.ts
@@ -5,7 +5,8 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'laravel/vite-plugin',
-		build: 'build',
-		test: 'test'
+		//env from https://github.com/laravel/vite-plugin/blob/73466441b0c9eb0c1a5ce0a0e937bd83eaef4b70/.github/workflows/tests.yml#L10
+		build: 'LARAVEL_BYPASS_ENV_CHECK=1 npm run build',
+		test: 'LARAVEL_BYPASS_ENV_CHECK=1 npm run test'
 	})
 }
