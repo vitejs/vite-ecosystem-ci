b12457acead6690a724570c37bee13745eb4c5a5
dominikg <dominik.goepel@gmx.de>
2022-10-06 12:15:22 +0200

    fix: use process.env instead of inline

--- tests/laravel.ts
+++ tests/laravel.ts
@@ -2,11 +2,12 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
-	await runInRepo({
+	//see https://github.com/laravel/vite-plugin/blob/73466441b0c9eb0c1a5ce0a0e937bd83eaef4b70/.github/workflows/tests.yml#L10
+	process.env.LARAVEL_BYPASS_ENV_CHECK="1";
+	runInRepo({
 		...options,
 		repo: 'laravel/vite-plugin',
-		//env from https://github.com/laravel/vite-plugin/blob/73466441b0c9eb0c1a5ce0a0e937bd83eaef4b70/.github/workflows/tests.yml#L10
-		build: 'LARAVEL_BYPASS_ENV_CHECK=1 npm run build',
-		test: 'LARAVEL_BYPASS_ENV_CHECK=1 npm run test'
+		build: 'build',
+		test: 'test'
 	})
 }
