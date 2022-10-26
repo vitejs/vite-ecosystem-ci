437e357cded6c9c4b094af71dfa76c4fdebec3aa
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-10-26 19:21:15 -0400

    Add storybook 7 test

--- tests/storybook.ts
+++ tests/storybook.ts
@@ -4,9 +4,14 @@ import { RunOptions } from '../types'
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
-		repo: 'storybookjs/builder-vite',
-		branch: 'main',
-		build: 'prepublish',
-		test: ['build-examples', 'test-ci'],
+		repo: 'storybookjs/repro-templates-temp',
+		dir: 'storybook7',
+		packageDir: 'react-vite/default-js/after-storybook',
+		branch: 'next',
+		build: ['yarn install', 'build-storybook'],
+		test: [
+			'yarn add --dev @storybook/test-runner http-server concurrently',
+			'npx concurrently -k "http-server storybook-static --port 6006 --silent" "npx wait-on http://localhost:6006 && npx test-storybook"',
+		],
 	})
 }
