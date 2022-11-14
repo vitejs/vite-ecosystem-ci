fb0ff45bd1cece4454ab473b26c5be90a25ed42b
dominikg <dominik.goepel@gmx.de>
2022-11-14 12:23:46 +0100

    feat: split svelte tests into vite-plugin-svelte and sveltekit

---
+++ tests/vite-plugin-svelte.ts
@@ -0,0 +1,16 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'sveltejs/vite-plugin-svelte',
+		branch: options.viteMajor === 4 ? 'vite-4' : 'main',
+		overrides: {
+			svelte: 'latest'
+		},
+		build: 'build',
+		beforeTest: 'pnpm playwright install chromium',
+		test: ['lint','test']
+	})
+}
