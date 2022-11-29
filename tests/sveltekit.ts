f853000d1d2d836d227866fe9a7eff7cc2bbfe45
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-11-29 08:49:31 -0800

    restore override. update comment

--- tests/sveltekit.ts
+++ tests/sveltekit.ts
@@ -19,7 +19,8 @@ export async function test(options: RunOptions) {
 		branch: options.viteMajor === 4 ? 'vite-4' : 'master',
 		overrides: {
 			svelte: 'latest',
-			'@sveltejs/vite-plugin-svelte': `${pluginPath}/packages/vite-plugin-svelte`
+			'@sveltejs/vite-plugin-svelte': `${pluginPath}/packages/vite-plugin-svelte`,
+			'@types/node': '^16.11.68' // override to kit's version to prevent ecosystem-ci override with vite-plugin-svelte version
 		},
 		beforeTest: 'pnpm playwright install',
 		test: ['lint', 'check', 'test']
