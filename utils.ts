5faf7a7f1ddc9f9fae7aeb72768f37f3956081aa
dominikg <dominik.goepel@gmx.de>
2022-12-05 18:00:01 +0100

    workaround: edit package.json to prevent use of pnpm 7.18

--- utils.ts
+++ utils.ts
@@ -377,7 +377,14 @@ export async function applyPackageOverrides(
 	const pm = agent?.split('@')[0]
 
 	const version = await $`${pm} --version`
-	console.warn(`overriding packages with ${pm}@${version}`)
+	if(pm === 'pnpm' && version === '7.18.0'){
+		console.warn('detected pnpm@7.18.0, changing pkg.packageManager and pkg.engines.pnpm to enforce use of pnpm@7.17.1')
+		pkg.packageManager='pnpm@7.17.1'
+		if(!pkg.engines) {
+			pkg.engines={}
+		}
+		pkg.engines.pnpm = '7.17.1'
+	}
 	if (pm === 'pnpm') {
 		if (!pkg.devDependencies) {
 			pkg.devDependencies = {}
