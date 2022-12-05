2e22fb63bcfb2e469638073601b9595c6c84a6ed
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-12-05 12:12:03 +0100

    feat: add histoire vite4 branch

--- tests/histoire.ts
+++ tests/histoire.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'histoire-dev/histoire',
+		branch: options.viteMajor === 4 ? 'feat/vite4' : 'main',
 		build: 'build',
 		test: ['test', 'test:examples'],
 	})
