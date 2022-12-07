1966cd4587cca795e0e7678400533b26d5892682
patak <matias.capeletto@gmail.com>
2022-12-07 09:47:03 +0100

    chore: vite-4 branch for Vitest

--- tests/vitest.ts
+++ tests/vitest.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vitest-dev/vitest',
+		branch: options.viteMajor === 4 ? 'vite-4' : 'main',
 		build: 'build',
 		test: 'test:run --allowOnly',
 	})
