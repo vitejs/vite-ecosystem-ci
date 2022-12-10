e3096a1ad08e09ff77eda68762dc6f85824baf52
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-12-11 05:23:29 +0530

    chore: update vitepress branch name (#176)
    * chore: update vitepress branch name
    
    * fix typo
    
    * skip test on vite 3

--- tests/vitepress.ts
+++ tests/vitepress.ts
@@ -2,13 +2,16 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
+	if (options.viteMajor < 4) {
+		return // no branch for vite 3
+	}
 	await runInRepo({
 		...options,
 		repo: 'vuejs/vitepress',
 		overrides: {
 			'@vitejs/plugin-vue': true,
 		},
-		branch: options.viteMajor === 4 ? 'vite-4' : 'main',
+		branch: 'main',
 		build: 'build',
 		test: 'test',
 	})
