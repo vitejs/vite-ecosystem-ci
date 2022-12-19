362d402d3e2f0a343da1662984ac5f8bba06b917
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-12-19 14:09:17 +0100

    chore: update vite 4 branch

--- tests/histoire.ts
+++ tests/histoire.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'histoire-dev/histoire',
-		branch: options.viteMajor === 4 ? 'feat/vite4' : 'main',
+		branch: 'main',
 		build: 'build',
 		test: ['test', 'test:examples'],
 	})
