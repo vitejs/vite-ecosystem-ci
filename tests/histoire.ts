b795cd4a4411e9e6a31e4738355e3cce584384ea
patak <matias.capeletto@gmail.com>
2022-12-19 14:23:08 +0100

    chore: update

--- tests/histoire.ts
+++ tests/histoire.ts
@@ -2,6 +2,9 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
+	if (options.viteMajor < 4) {
+		return // no branch for vite 3
+	}
 	await runInRepo({
 		...options,
 		repo: 'histoire-dev/histoire',
