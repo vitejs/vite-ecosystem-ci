0e5a31cc978edead0e9d0ad85c95c7188c7b0acf
Brandon Roberts <robertsbt@gmail.com>
2022-12-30 10:28:57 -0600

    chore: update branch

--- tests/analog.ts
+++ tests/analog.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'analogjs/analog',
-		branch: 'main',
+		branch: 'vite-3-ci',
 		build: 'build:vite-ci',
 		test: 'test:vite-ci',
 	})
