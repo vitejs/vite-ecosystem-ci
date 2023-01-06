e24e95076c02eaf68ffacef652223336ac9fec74
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2023-01-06 15:00:45 +0100

    feat: add vtu (#1)

---
+++ tests/vtu.ts
@@ -0,0 +1,14 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: '@vuejs/test-utils',
+		overrides: {
+			'vue-tsc': true,
+		},
+		branch: 'main',
+		test: 'vue-tsc',
+	})
+}
