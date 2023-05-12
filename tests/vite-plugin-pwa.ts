accb439fa3f8dab413f42aca376bb759e4437c48
userquin <userquin@gmail.com>
2023-05-12 23:08:25 +0200

    fix: only vite 4 (assets hash name now using rollup default)

--- tests/vite-plugin-pwa.ts
+++ tests/vite-plugin-pwa.ts
@@ -2,6 +2,12 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
+	if (options.viteMajor < 4) {
+		// no branch with 3.0 version
+		// ts test will fail since asset hash separator now is rollup default (-) instead dot (.)
+		return
+	}
+
 	await runInRepo({
 		...options,
 		repo: 'vite-pwa/vite-plugin-pwa',
