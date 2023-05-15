ffd0ce1acc44d8b2cfeb010a78c99061b66e6c78
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-05-15 10:44:15 +0300

    feat(tests): add Nx test suite (#211)

---
+++ tests/nx.ts
@@ -0,0 +1,12 @@
+import { runInRepo } from '../utils'
+import { RunOptions } from '../types'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'nrwl/nx',
+		branch: 'master',
+		build: 'build-project vite --skip-nx-cache',
+		test: ['test vite --skip-nx-cache', 'e2e e2e-vite --skip-nx-cache'],
+	})
+}
