e7729cb46dfd348aed8353cb82b6171e998792b6
userquin <userquin@gmail.com>
2023-05-19 15:46:36 +0200

    chore: update branch to main

--- tests/vite-plugin-pwa.ts
+++ tests/vite-plugin-pwa.ts
@@ -10,7 +10,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'vite-pwa/vite-plugin-pwa',
-		branch: 'userquin/feat-add-tests',
+		branch: 'main',
 		beforeTest: 'pnpm playwright install chromium',
 		build: 'build',
 		test: 'test:vite-ecosystem-ci',
