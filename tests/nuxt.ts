f318ebc7787b82154999663802ab5321214eef50
Dominik G <dominik.goepel@gmx.de>
2023-09-18 13:01:19 +0200

    fix: use playwright-core
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- tests/nuxt.ts
+++ tests/nuxt.ts
@@ -12,7 +12,7 @@ export async function test(options: RunOptions) {
 			'@vitejs/plugin-vue': true,
 		},
 		build: 'build',
-		beforeTest: 'pnpm playwright install',
+		beforeTest: 'pnpm playwright-core install',
 		test: ['test:fixtures', 'test:types'],
 	})
 }
