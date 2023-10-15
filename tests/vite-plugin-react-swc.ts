f537f1d6f5e53186e9c78f52e7a31edbe0ed8119
dominikg <dominik.goepel@gmx.de>
2023-10-15 12:56:15 +0200

    chore: remove special handling for vite-3

--- tests/vite-plugin-react-swc.ts
+++ tests/vite-plugin-react-swc.ts
@@ -2,7 +2,6 @@ import { runInRepo } from '../utils'
 import { RunOptions } from '../types'
 
 export async function test(options: RunOptions) {
-	if (options.viteMajor < 4) return
 	await runInRepo({
 		...options,
 		repo: 'vitejs/vite-plugin-react-swc',
