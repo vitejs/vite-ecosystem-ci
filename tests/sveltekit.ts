3eddf463d081e7f41f2113778e2e44638b87a063
Dominik G <dominik.goepel@gmx.de>
2023-12-19 17:52:51 +0100

    chore: update sveltekit default branch (#9)

--- tests/sveltekit.ts
+++ tests/sveltekit.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'sveltejs/kit',
-		branch: 'version-2',
+		branch: 'main',
 		overrides: {
 			'@sveltejs/vite-plugin-svelte': true,
 			'@sveltejs/vite-plugin-svelte-inspector': true,
