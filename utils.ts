5e4823d0ce7e5edb3b826a9982f35ad0c9973a6b
dominikg <dominik.goepel@gmx.de>
2024-01-27 11:41:08 +0100

    fix(pnpm): do not prefer local, it could be tainted

--- utils.ts
+++ utils.ts
@@ -532,7 +532,7 @@ export async function applyPackageOverrides(
 
 	// use of `ni` command here could cause lockfile violation errors so fall back to native commands that avoid these
 	if (pm === 'pnpm') {
-		await $`pnpm install --prefer-frozen-lockfile --prefer-offline --strict-peer-dependencies false`
+		await $`pnpm install --prefer-frozen-lockfile --strict-peer-dependencies false`
 	} else if (pm === 'yarn') {
 		await $`yarn install`
 	} else if (pm === 'npm') {
