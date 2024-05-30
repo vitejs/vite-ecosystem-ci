9be20d5d611ac8883b1310003e1479137334c07d
Valentin Palkovic <valentin@chromatic.com>
2024-05-30 12:17:13 +0200

    chore: update test configuration for storybook
    Update the test configuration in the storybook.ts file to use the 'next' branch of the 'storybookjs/sandboxes' repository. This change ensures that the correct test subdirectory is used for the React Vite default TypeScript setup. Additionally, the 'beforeTest' and 'test' functions have been updated to use the 'getCommand' function from the '@antfu/ni' package for executing commands. These changes improve the reliability and flexibility of the test setup and considers the used package manager

--- tests/storybook.ts
+++ tests/storybook.ts
@@ -1,17 +1,22 @@
-import { runInRepo } from '../utils.ts'
+import { $, runInRepo } from '../utils.ts'
 import type { RunOptions } from '../types.d.ts'
+import { getCommand } from '@antfu/ni'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
-		repo: 'storybookjs/repro-templates-temp',
-		dir: 'storybook7',
-		testSubdirectory: 'react-vite/default-js/after-storybook',
+		repo: 'storybookjs/sandboxes',
+		dir: 'next',
+		testSubdirectory: 'react-vite/default-ts/after-storybook',
 		branch: 'next',
 		build: ['build-storybook'],
-		test: [
-			'yarn add --dev @storybook/test-runner http-server concurrently',
-			'npx concurrently -k "http-server storybook-static --port 6006 --silent" "npx wait-on http://localhost:6006 && npx test-storybook"',
-		],
+		beforeTest: async (agent) => {
+			$`${getCommand(agent, 'execute', ['http-server', 'storybook-static', '--port 6006', '--silent'])}`
+			await $`${getCommand(agent, 'execute', ['wait-on', 'http://localhost:6006'])}`
+		},
+		test: async (agent) => {
+			await $`${getCommand(agent, 'execute', ['@storybook/test-runner'])}`
+			await $`${getCommand(agent, 'execute', ['fkill-cli', ':6006'])}`
+		},
 	})
 }
