befa3af7b400be9bcbd7215737e57e9461f4106b
翠 / green <green@sapphi.red>
2024-09-30 16:11:18 +0900

    fix(vuepress): install playwright chromium before testing (#328)

--- tests/vuepress.ts
+++ tests/vuepress.ts
@@ -10,6 +10,7 @@ export async function test(options: RunOptions) {
 		},
 		branch: 'main',
 		build: 'build',
+		beforeTest: 'pnpm --filter e2e exec playwright install chromium',
 		test: 'test',
 	})
 }
