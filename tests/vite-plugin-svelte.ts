3c1eed690c03c11dbb8f6b25b92d8076cb949118
dominikg <dominik.goepel@gmx.de>
2024-10-21 19:47:22 +0200

    fix: svelte5 has been released, switch vite-plugin-svelte to main branch again

--- tests/vite-plugin-svelte.ts
+++ tests/vite-plugin-svelte.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'sveltejs/vite-plugin-svelte',
-		branch: 'v3',
+		branch: 'main',
 		overrides: {
 			svelte: 'latest',
 		},
