297e265862831183e56d56010d8258f0827ba917
Mohammad Bagher Abiyat <zorofight94@gmail.com>
2024-11-05 08:24:24 +0330

    temporary debug

--- tests/waku.ts
+++ tests/waku.ts
@@ -4,10 +4,10 @@ import type { RunOptions } from '../types.d.ts'
 export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
-		repo: 'dai-shi/waku',
-		branch: 'dev',
-		build: 'build',
-		beforeTest: 'pnpm playwright-core install',
-		test: 'e2e',
+		repo: 'Aslemammad/waku',
+		branch: 'chore/HMRBroadcaster-type',
+		build: 'compile',
+		beforeTest: 'pnpm playwright install',
+		test: ['pnpm run --filter waku test', 'e2e'],
 	})
 }
