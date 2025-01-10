8ad9e9c33611a0f8d2cda2f0ee9f2484cb962f6f
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-01-10 12:24:28 +0900

    chore: tweak

--- tests/rakkas.ts
+++ tests/rakkas.ts
@@ -1,5 +1,6 @@
 import { runInRepo } from '../utils.ts'
 import type { RunOptions } from '../types.d.ts'
+import { execSync } from 'node:child_process'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
@@ -10,7 +11,11 @@ export async function test(options: RunOptions) {
 		// This is needed to run puppeteer in Ubuntu 23+
 		// https://github.com/puppeteer/puppeteer/pull/13196
 		beforeTest: process.env.GITHUB_ACTIONS
-			? 'bash -c "echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns"'
+			? async () => {
+					execSync(
+						'echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns',
+					)
+				}
 			: undefined,
 		test: 'vite-ecosystem-ci',
 	})
