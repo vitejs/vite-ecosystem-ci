2e0901994921e7cb94ec35d80bb47907b535392f
翠 / green <green@sapphi.red>
2025-01-10 17:19:44 +0900

    fix(rakkas): disable apparmor restrictions on GitHub Actions for now (#350)

--- tests/rakkas.ts
+++ tests/rakkas.ts
@@ -1,5 +1,6 @@
 import { runInRepo } from '../utils.ts'
 import type { RunOptions } from '../types.d.ts'
+import { execSync } from 'node:child_process'
 
 export async function test(options: RunOptions) {
 	await runInRepo({
@@ -7,6 +8,15 @@ export async function test(options: RunOptions) {
 		repo: 'rakkasjs/rakkasjs',
 		branch: 'main',
 		build: 'build',
+		// This is needed to run puppeteer in Ubuntu 23+
+		// https://github.com/puppeteer/puppeteer/pull/13196
+		beforeTest: process.env.GITHUB_ACTIONS
+			? async () => {
+					execSync(
+						'echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns',
+					)
+				}
+			: undefined,
 		test: 'vite-ecosystem-ci',
 	})
 }
