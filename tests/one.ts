fa14d58fcd75fcc269eb54d2f05c009831d95ea1
dominikg <dominik.goepel@gmx.de>
2025-03-11 21:12:04 +0100

    chore: fix formatting

--- tests/one.ts
+++ tests/one.ts
@@ -2,12 +2,12 @@ import type { RunOptions } from '../types.d.ts'
 import { runInRepo } from '../utils.ts'
 
 export async function test(options: RunOptions) {
-  await runInRepo({
-    ...options,
-    repo: 'onejs/one',
-    branch: 'main',
-    build: 'build',
-    beforeTest: 'yarn playwright install chromium',
-    test: 'test:vite-ecosystem-ci',
-  })
+	await runInRepo({
+		...options,
+		repo: 'onejs/one',
+		branch: 'main',
+		build: 'build',
+		beforeTest: 'yarn playwright install chromium',
+		test: 'test:vite-ecosystem-ci',
+	})
 }
