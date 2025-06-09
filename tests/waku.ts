a70edf00815ef626f5645ac3562d01eb24290a73
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-09 11:55:59 +0900

    chore: skip for waku

--- tests/waku.ts
+++ tests/waku.ts
@@ -9,5 +9,13 @@ export async function test(options: RunOptions) {
 		build: 'compile',
 		beforeTest: 'pnpm playwright install chromium',
 		test: 'test-vite-ecosystem-ci',
+		overrides: {
+			// It uses Vitest 3.2+ so we don't need to inject the overrides.
+			// If we inject overrides, the following error happens due to how waku sets overrides for the test.
+			//
+			// npm error code EINVALIDTAGNAME
+			// npm error Invalid tag name "<3.2.0>vite" of package "vitest@<3.2.0>vite": Tags may not have any characters that encodeURIComponent encodes.
+			vitest: false,
+		},
 	})
 }
