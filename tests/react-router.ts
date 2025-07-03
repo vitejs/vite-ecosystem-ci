25acf93771c7c1cd73d8a450bb892984d023976d
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-03 15:26:33 +0900

    chore: remove vite-node overrides

--- tests/react-router.ts
+++ tests/react-router.ts
@@ -9,9 +9,5 @@ export async function test(options: RunOptions) {
 		build: 'vite-ecosystem-ci:build',
 		beforeTest: 'vite-ecosystem-ci:before-test',
 		test: 'vite-ecosystem-ci:test',
-		overrides: {
-			// For Vite 7 support
-			'vite-node': '^3.2.2',
-		},
 	})
 }
