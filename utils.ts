d6cee0abadf07fee7d14582b56ef74d3c5af22f6
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-09 10:55:09 +0900

    fix: set overrides without special syntax to devDeps
    Co-authored-by: "Dominik G." <dominik.goepel@gmx.de>
    Co-authored-by: CokaKoala <31664583+AdrianGonz97@users.noreply.github.com>

--- utils.ts
+++ utils.ts
@@ -555,7 +555,7 @@ export async function applyPackageOverrides(
 		const overridesWithoutSpecialSyntax = Object.fromEntries(
 			Object.entries(overrides)
 				//eslint-disable-next-line @typescript-eslint/no-unused-vars
-				.filter(([key, value]) => (value as string).includes('>')),
+				.filter(([key, value]) => !key.includes('>')),
 		)
 
 		if (!pkg.devDependencies) {
