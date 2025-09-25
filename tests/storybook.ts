8f98dc5fa23beaaf321dd82b0a7d440e17d9ccb4
Jeppe Reinhold <jeppe@reinhold.is>
2025-09-25 17:02:06 +0200

    add storybook test

---
+++ tests/storybook.ts
@@ -0,0 +1,13 @@
+import { runInRepo } from '../utils.ts'
+import type { RunOptions } from '../types.d.ts'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'storybookjs/storybook',
+		branch: 'svelte-ecosystem-ci',
+		build: 'svelte-ecosystem-ci:build',
+		beforeTest: 'svelte-ecosystem-ci:before-test',
+		test: 'svelte-ecosystem-ci:test',
+	})
+}
