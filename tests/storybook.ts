ac0483983c23ef358f19278d87a53607e41284f2
Jeppe Reinhold <jeppe@reinhold.is>
2025-09-29 11:22:29 +0200

    Update tests/storybook.ts

--- tests/storybook.ts
+++ tests/storybook.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'storybookjs/storybook',
-		branch: 'svelte-ecosystem-ci',
+		branch: 'next',
 		build: 'svelte-ecosystem-ci:build',
 		beforeTest: 'svelte-ecosystem-ci:before-test',
 		test: 'svelte-ecosystem-ci:test',
