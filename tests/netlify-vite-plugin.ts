53edb4ecb49d4940be89ef68c1b69edef58701fc
Philippe Serhal <philippe.serhal@gmail.com>
2026-03-09 08:42:42 -0400

    refactor: don't set `agent` explicitly
    Co-authored-by: Dominik G. <dominik.goepel@gmx.de>

--- tests/netlify-vite-plugin.ts
+++ tests/netlify-vite-plugin.ts
@@ -5,7 +5,6 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'netlify/primitives',
-		agent: 'npm',
 		build: 'npm run build --workspaces',
 		beforeTest: 'npx playwright install chromium',
 		test: ['npm run test -w packages/vite-plugin'],
