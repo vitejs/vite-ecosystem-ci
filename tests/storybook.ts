15a43a99558846a170039587abff69013c901dec
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-03-10 14:40:26 +0900

    chore: update expected failures status

--- tests/storybook.ts
+++ tests/storybook.ts
@@ -11,7 +11,3 @@ export async function test(options: RunOptions) {
 		test: 'vite-ecosystem-ci:test',
 	})
 }
-
-export const rolldownViteExpectedFailureReason = `
-needs to be updated on storybook side ("yarn install" errors with peer dep errors)
-`
