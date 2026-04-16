1cea77c94384cc6b37f072362f4d4b43e4bdeb38
gioboa <giorgiob.boa@gmail.com>
2026-04-16 15:47:48 +0200

    feat: add module-federation/vite

---
+++ tests/vite-module-federation.ts
@@ -0,0 +1,10 @@
+import { runInRepo } from '../utils.ts'
+import type { RunOptions } from '../types.d.ts'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'module-federation/vite',
+		test: 'pnpm test',
+	})
+}
