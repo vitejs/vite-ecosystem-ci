5e2094f90cb73f5be4036a5963f46ecd03e13fb4
翠 <green@sapphi.red>
2026-06-16 17:46:21 +0900

    fix(react-router): switch to main branch (#475)

--- tests/react-router.ts
+++ tests/react-router.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'remix-run/react-router',
-		branch: 'dev',
+		branch: 'main',
 		build: 'vite-ecosystem-ci:build',
 		beforeTest: 'vite-ecosystem-ci:before-test',
 		test: 'vite-ecosystem-ci:test',
