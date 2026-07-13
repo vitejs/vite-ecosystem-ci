ff516c670dbdca8b5c22a9f2b21ace7693b8a337
Tee Ming <chewteeming01@gmail.com>
2026-07-13 18:54:24 +0800

    fix: install Playwright for layerchart `test:ui` in CI (#66)

--- tests/layerchart.ts
+++ tests/layerchart.ts
@@ -5,6 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'techniq/layerchart',
+		beforeTest: 'pnpm playwright install',
 		build: 'pnpm --dir packages/layerchart build',
 		test: [
 			'pnpm --dir packages/layerchart test:unit',
