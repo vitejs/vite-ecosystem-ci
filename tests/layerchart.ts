647d30fb50b8314e9ac3f8667b59534eaefcaafd
Tee Ming <chewteeming01@gmail.com>
2026-07-15 17:19:05 +0800

    specify dir (#67)

--- tests/layerchart.ts
+++ tests/layerchart.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'techniq/layerchart',
-		beforeTest: 'pnpm playwright install',
+		beforeTest: 'pnpm --dir packages/layerchart playwright install',
 		build: 'pnpm --dir packages/layerchart build',
 		test: [
 			'pnpm --dir packages/layerchart test:unit',
