ff344dc60600df7ef33c95cdf51f3c3e4aeb1c34
Tee Ming <chewteeming01@gmail.com>
2026-07-25 18:02:28 +0800

    fix(sveltekit): switch to `version-3` branch (#500)

--- tests/sveltekit.ts
+++ tests/sveltekit.ts
@@ -5,7 +5,7 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'sveltejs/kit',
-		branch: 'main',
+		branch: 'version-3',
 		overrides: {
 			svelte: 'latest',
 			'@sveltejs/vite-plugin-svelte': true,
