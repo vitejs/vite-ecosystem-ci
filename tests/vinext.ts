95d389c3de50a1325b9d83d29d0d7006b8f915d7
James <james@eli.cx>
2026-08-07 10:49:56 +0100

    feat: add vinext

---
+++ tests/vinext.ts
@@ -0,0 +1,13 @@
+import { $, runInRepo } from '../utils.ts'
+import type { RunOptions } from '../types.d.ts'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'cloudflare/vinext',
+		branch: 'main',
+		beforeInstall: async () => $`pnpm config set frozen-lockfile false --location project`,
+		build: 'vite-ecosystem-ci:build',
+		test: 'vite-ecosystem-ci:test',
+	})
+}
