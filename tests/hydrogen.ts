01de03382ff367046b855991e85fa71c71bee204
sapphi-red <green@sapphi.red>
2026-08-21 13:18:17 +0900

    refactor: generate suite list with a script

--- tests/hydrogen.ts
+++ tests/hydrogen.ts
@@ -1,5 +1,10 @@
 import { runInRepo } from '../utils.ts'
-import type { RunOptions } from '../types.d.ts'
+import type { DisableWorkflow, RunOptions } from '../types.d.ts'
+
+export const disableWorkflow: DisableWorkflow = {
+	scheduled: 'disabled until they complete they migration back to Vite',
+	selected: 'disabled until they complete they migration back to Vite',
+}
 
 export async function test(options: RunOptions) {
 	await runInRepo({
--- tests/nx.ts
+++ tests/nx.ts
@@ -1,5 +1,9 @@
 import { runInRepo } from '../utils.ts'
-import type { RunOptions } from '../types.d.ts'
+import type { DisableWorkflow, RunOptions } from '../types.d.ts'
+
+export const disableWorkflow: DisableWorkflow = {
+	scheduled: 'disabled temporarily',
+}
 
 export async function test(options: RunOptions) {
 	await runInRepo({
